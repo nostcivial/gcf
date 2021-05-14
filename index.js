3fbfef7165629ceab98ec134c3c106e93798a391
nostcivial <81406180+nostcivial@users.noreply.github.com>
2021-05-14 14:56:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ exports.gcfSendGrid = (data, context) => {
   const msg = {
     to: 'TOADDRESS@gmail.com',
     from: 'FROMADDRESS@gmail.com',
-    subject: 'Learn GCP with Mahesh - Send Emails from Cloud Functions with SendGrid using Nodejs8',
-    html: 'Hello <strong>Mahesh</strong>, <br/>The file which was upload to <strong>'+data.bucket+'</strong> is <strong>'+data.name +'</strong>.<br/>Thanks<br/> Learn GCP with Mahesh'      
+    subject: 'Learn GCP with Marcus - Send Emails from Cloud Functions with SendGrid using Nodejs8',
+    html: 'Hello <strong>Marcus</strong>, <br/>The file which was upload to <strong>'+data.bucket+'</strong> is <strong>'+data.name +'</strong>.<br/>Thanks<br/> Learn GCP with Marcus'      
   };
   sendGridMail.send(msg)
 };
